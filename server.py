dc9a6114d1efec4d98d87f988309175d1284837c
Robert-Noland <quaidnoland4@gmail.com>
2025-05-21 19:07:47 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import selectors
 import threading
 
 
@@ -7,49 +8,51 @@ ip = socket.gethostbyname(hostname)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((ip,5555))
-server.listen(5)
+#sel = selectors.DefaultSelector()
+#server.setblocking(False)
+#sel.register(server, selectors.EVENT_READ, data=None)
 
 
 #Clients dict used to store info about client connection
-#global clients
 clients = {}
-#usernames = []
 
 
-#adding this to move the commented section under receive to here
-def handle_clients(conn)
-
-#Recieve method 
-def receive():
-    while True:
-        msg = conn.recv(1024)
-        broadcast(msg, name + ":")
-        #client, address = server.accept()
-        # Asks the clients for their Usernames
-        #client.send('USERNAME'.encode('ascii'))
-        #username = client.recv(1024).decode('ascii')
-        #print(f"Connected with {username} {str(address)}")
-    
-        
-
-        #usernames.append(username)
-        #clients.append(client)
 #Broadcasting Method
-def broadcast(msg, prefix=""):
-    for client in clients:
-        client.send(bytes(prefix,"utf-8") + msg)
+def broadcast():
+   # Iterates through the clients dictionary and sends the message to each client
+   for client in clients:
+      try:
+         client.send(msg.encode('utf-8'))
+      except Exception as e:
+         print(f"Error sending message to {clients[client]}: {e}")
+
+def accept_connection():
+   while True:
+    global client, address
+    client, address = server.accept()
+    clients[client] = address
+    print(f"Connected with {str(address)}")
+    #calls the handle clients function with a thread for handling multiple clients
+    threading.Thread(target=handle_client, args=(client,)).start()
+
+def handle_client(client):
+    #Receives messages in a loop and broadcasts them 
+     while True:
+       global msg
+       msg = client.recv(1024).decode('utf-8')
+       print(f'{str(address)} {msg}' )
+       broadcast()
+       if not msg:
+        break
        
+if __name__ == "__main__":
+   #Accepts 20 clients at once at the most
+   server.listen(20)
+   print('ROOM IS OPEN ...')
+   #Calls the handle_clients function with a thread for handling multiple requests at once
+   t = threading.Thread(target=accept_connection)
+   t.start()
+   t.join()
 
 
-
-
-
-
-threading.Thread(target=broadcast).start()
-threading.Thread(target=receive).start()
-
-
-#Calls the recieve method
-print('ROOM IS OPEN ...')
-receive()
-broadcast()
+#threading.Thread(target=broadcast).start()

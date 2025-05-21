2ff370a0071b1ac74b9b370ebf756f8f6d0fc4ad
Robert-Noland <quaidnoland4@gmail.com>
2025-05-21 19:08:48 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,48 +1,42 @@
 import socket
 import threading
 
+global stop_thread
 stop_thread = False
 
 def join_server():
-    global username 
-    username = input("Please enter your username: ")
+    global name 
+    name = input("Please enter your username: ")
     global client
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    hostip = input("Please enter the host's IP: ")
-    client.connect((hostip,5555))
+    #hostip = input("Please enter the host's IP: ")
+    client.connect(('10.5.0.2',5555))
     
         
         
 
     
-def receiving_messages():
+def receiving_messages(client):
     while True:
-        global stop_thread
-        if stop_thread:
-            break
         try:
-            nameid = client.recv(1024).decode('ascii')
-            if nameid == 'USERNAME':
-                client.send(username.encode('ascii'))
-            else:
-                message = client.recv(1024).decode('utf-8')
-                print(message)
-        except socket.error:
-            print('Error Occured while Connecting')
-            client.close()
+            data = client.recv(1024)
+            message = data.decode('utf-8')
+            print(message)
+        except Exception as e:
+            print(f"There is an Error Receiving Message: {e}")
             break
+        client.close()
 
-def sending_messages():
+
+def sending_messages(client):
     while True:     
-        message = f'{username}: {input("")}'
-        client.send(message.encode('utf-8'))
+        msg = f'{name}: {input("")}'
+        client.send(msg.encode('utf-8'))
 
 
 
 
 join_server()
-sending_messages()
-receiving_messages()
       
-threading.Thread(target=sending_messages, args=(client,)).start()
-threading.Thread(target=receiving_messages, args=(client,)).start()   
+threading.Thread(target=sending_messages(client), args=(client)).start()
+threading.Thread(target=receiving_messages(client), args=(client)).start()   

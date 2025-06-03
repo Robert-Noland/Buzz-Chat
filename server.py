6cad0017181981f7289c82cf9098d84f8cf0b1d3
Robert-Noland <quaidnoland4@gmail.com>
2025-06-03 12:59:50 -0400

    Updated server 50%.py
    it now accepts multiple connections, broadcasts messages, broadcasts when a user leaves and join and the server prints who leaves and who joins

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket
-import selectors
 import threading
 
 
@@ -8,17 +7,14 @@ ip = socket.gethostbyname(hostname)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((ip,5555))
-#sel = selectors.DefaultSelector()
-#server.setblocking(False)
-#sel.register(server, selectors.EVENT_READ, data=None)
 
 
-#Clients dict used to store info about client connection
-clients = {}
+#A Clients list is used to store info about client connection
+clients = []
 
 
 #Broadcasting Method
-def broadcast():
+def broadcast(msg):
    # Iterates through the clients dictionary and sends the message to each client
    for client in clients:
       try:
@@ -30,29 +26,39 @@ def accept_connection():
    while True:
     global client, address
     client, address = server.accept()
-    clients[client] = address
+    clients.append(client)
     print(f"Connected with {str(address)}")
-    #calls the handle clients function with a thread for handling multiple clients
+    broadcast(f"A new user has connected!")
+    #calls the handle_client function with a thread for handling multiple clients
     threading.Thread(target=handle_client, args=(client,)).start()
 
 def handle_client(client):
     #Receives messages in a loop and broadcasts them 
      while True:
-       global msg
-       msg = client.recv(1024).decode('utf-8')
-       print(f'{str(address)} {msg}' )
-       broadcast()
-       if not msg:
-        break
+      try:
+         global msg
+         msg = client.recv(1024).decode('utf-8')
+         print(f'{str(address)} {msg}' )
+         broadcast(msg)
+      except Exception as e:
+            if client in clients:
+                # Index is used to remove client from list after getting disconnected
+                index = clients.index(client)
+                print(f"Lost connection with {str(address)}")
+                clients.remove(client)
+                broadcast('A user has disconnected!')
+                client.close()
+                break
+
        
 if __name__ == "__main__":
    #Accepts 20 clients at once at the most
    server.listen(20)
    print('ROOM IS OPEN ...')
-   #Calls the handle_clients function with a thread for handling multiple requests at once
+   #Calls the accept_connection function with a thread for handling multiple requests at once
    t = threading.Thread(target=accept_connection)
    t.start()
    t.join()
 
 
-#threading.Thread(target=broadcast).start()
+threading.Thread(target=broadcast, args=(msg,)).start()
